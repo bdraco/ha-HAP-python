78c1b7caf783723fcfba22cac68acf12a9829489
J. Nick Koston <nick@cpanel.net>
2019-12-03 12:02:17 -0600

    adj

--- pyhap/accessory_driver.py
+++ pyhap/accessory_driver.py
@@ -450,7 +450,7 @@ class AccessoryDriver:
             subscribed_clients = self.topics.get(topic, [])
             logger.debug('Send event: topic(%s), data(%s), sender_client_addr(%s)', topic, bytedata, sender_client_addr)
             for client_addr in subscribed_clients.copy():
-                if sender_client_addr and sender_client_addr == sender_client_addr:
+                if sender_client_addr and sender_client_addr == client_addr:
                     logger.debug('Skip sending event to client since its the client that made the characteristic change: %s', client_addr)
                     continue
                 logger.debug('Sending event to client: %s', client_addr)
