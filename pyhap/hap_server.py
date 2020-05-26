aac64be8728ce704bcfb489f72afef0c20a2391a
J. Nick Koston <nick@koston.org>
2020-05-26 20:12:58 +0000

    more debug

--- pyhap/hap_server.py
+++ pyhap/hap_server.py
@@ -163,6 +163,44 @@ class HAPServerHandler(BaseHTTPRequestHandler):
 
         super(HAPServerHandler, self).__init__(sock, client_addr, server)
 
+    def handle_one_request(self):
+        """Handle a single HTTP request.
+
+        You normally don't need to override this method; see the class
+        __doc__ string for information on how to handle specific HTTP
+        commands such as GET and POST.
+
+        """
+        try:
+            self.raw_requestline = self.rfile.readline(65537)
+            if len(self.raw_requestline) > 65536:
+                self.requestline = ''
+                self.request_version = ''
+                self.command = ''
+                self.send_error(HTTPStatus.REQUEST_URI_TOO_LONG)
+                return
+            if not self.raw_requestline:
+                self.close_connection = True
+                return
+            if not self.parse_request():
+                # An error code has been sent, just exit
+                return
+            mname = 'do_' + self.command
+            if not hasattr(self, mname):
+                self.send_error(
+                    HTTPStatus.NOT_IMPLEMENTED,
+                    "Unsupported method (%r)" % self.command)
+                return
+            method = getattr(self, mname)
+            method()
+            self.wfile.flush() #actually send the response if not already done.
+        except socket.timeout as e:
+            #a read or a write timed out.  Discard this connection
+            logger.exception("Unexpected socket timeout")
+            self.log_error("Request timed out: %r", e)
+            self.close_connection = True
+            return
+
     def log_message(self, format, *args):  # pylint: disable=redefined-builtin
         logger.info("%s - %s", self.address_string(), format % args)
 
